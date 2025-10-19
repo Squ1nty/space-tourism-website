9e4acab43fa6c88b4ab27efef09d85116490c993
Vincent Dao <vincentdao288@gmail.com>
2025-10-19 21:29:35 +1100

    Adding yml file along with minor changes to vite config and package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'bookmark-landing-page-master' : '/',
+  base: process.env.NODE_ENV === 'production' ? 'space-tourism-website' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
