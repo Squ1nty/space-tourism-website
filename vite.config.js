fd85a6cd67a232f9f9cb9a74718bdcd8be6c67f8
Vincent Dao <vincentdao288@gmail.com>
2025-11-09 19:58:41 +1100

    ^

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? 'space-tourism-website' : '/',
   build: {
-    outDir: 'dist',
+    outDir: 'root',
     assetsDir: 'assets'
   }
 })
