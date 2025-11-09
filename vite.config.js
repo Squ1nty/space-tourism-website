91b882f5ae1d29debb92c6cd061ec522ded9eec6
Vincent Dao <vincentdao288@gmail.com>
2025-11-09 20:15:49 +1100

    Properly deployed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? 'space-tourism-website' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/space-tourism-website/' : '/',
   build: {
     outDir: 'root',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        destination: './starter-code/html/destination.html',
+        crew: './starter-code/html/crew.html',
+        technology: './starter-code/html/technology.html',
+      }
+    }
   }
 })
