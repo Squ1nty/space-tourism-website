1a9e92dd27b09aba78a57e274da1a72034a6f0ea
Vincent Dao <vincentdao288@gmail.com>
2025-11-09 19:25:45 +1100

    ^

--- starter-code/js/crew.js
+++ starter-code/js/crew.js
@@ -40,13 +40,13 @@ function updateCurrentCrewState(number){
 // Handles major screen size changes upon initial load and any resizing
 function handleBackgroundImg(){
   if(window.innerWidth >= 768 && window.innerWidth < 1440){
-    bgImg.setAttribute("src", "/starter-code/assets/crew/background-crew-tablet.jpg");
+    bgImg.setAttribute("src", "/space-tourism-website/starter-code/assets/crew/background-crew-tablet.jpg");
   }
   else if(window.innerWidth >= 1440){
-    bgImg.setAttribute("src", "/starter-code/assets/crew/background-crew-desktop.jpg");
+    bgImg.setAttribute("src", "/space-tourism-website/starter-code/assets/crew/background-crew-desktop.jpg");
   }
   else if(window.innerWidth < 768){
-    bgImg.setAttribute("src", "/starter-code/assets/crew/background-crew-mobile.jpg");
+    bgImg.setAttribute("src", "/space-tourism-website/starter-code/assets/crew/background-crew-mobile.jpg");
   }
 }
 window.addEventListener("load", () => {
