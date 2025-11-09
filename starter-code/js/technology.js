c80fa58d78b990da3dc0bd19c6b519a13707a786
Vincent Dao <vincentdao288@gmail.com>
2025-11-09 18:37:05 +1100

    Complete.

--- starter-code/js/technology.js
+++ starter-code/js/technology.js
@@ -1,16 +1,25 @@
 let bgImg = document.querySelector(".body__bgImg");
 let heroImgs = document.querySelectorAll(".tech__heroImg");
+let textBoxes = document.querySelectorAll(".tech__textBox");
 let btns = document.querySelectorAll(".tech__slideBtn");
 let btnsArray = Array.from(btns);
 
 function handleContentState(index){
-  
+  heroImgs.forEach(img => {
+    img.classList.remove("active");
+  });
+  textBoxes.forEach(box => {
+    box.classList.remove("active");
+  });
+  heroImgs[index].classList.add("active");
+  textBoxes[index].classList.add("active");
 }
 function handleActiveBtn(index){
   btns.forEach(btn => {
     btn.classList.remove("active");
   });
   btns[index].classList.add("active");
+  handleContentState(index);
 }
 btns.forEach(btn => {
   btn.addEventListener("click", (e) => {
@@ -22,13 +31,13 @@ btns.forEach(btn => {
 });
 // Handles major screen size changes upon initial load and any resizing
 function handleImgs(){
-  if(window.innerWidth >= 768 && window.innerWidth < 1440){
+  if(window.innerWidth >= 768 && window.innerWidth < 1024){
     bgImg.setAttribute("src", "../assets/technology/background-technology-tablet.jpg");
     heroImgs.forEach(img => {
       img.setAttribute("src", `../assets/technology/image-${img.classList[1]}-landscape.jpg`);
     });
   }
-  else if(window.innerWidth >= 1440){
+  else if(window.innerWidth >= 1024){
     bgImg.setAttribute("src", "../assets/technology/background-technology-desktop.jpg");
     heroImgs.forEach(img => {
       img.setAttribute("src", `../assets/technology/image-${img.classList[1]}-portrait.jpg`);
