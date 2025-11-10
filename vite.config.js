2b18c3d903ac9a480899198c79668a5383914e37
Vincent Dao <vincentdao288@gmail.com>
2025-11-10 13:45:52 +1100

    ^

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/',
+  base: '/', // Simpler for Vercel
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Recommended for Vercel
     assetsDir: 'assets',
     rollupOptions: {
       input: {
-        main: './index.html',
-        destination: './starter-code/html/destination.html',
-        crew: './starter-code/html/crew.html',
-        technology: './starter-code/html/technology.html',
+        main: resolve(__dirname, 'index.html'),
+        destination: resolve(__dirname, 'starter-code/html/destination.html'),
+        crew: resolve(__dirname, 'starter-code/html/crew.html'),
+        technology: resolve(__dirname, 'starter-code/html/technology.html'),
       }
     }
   }
